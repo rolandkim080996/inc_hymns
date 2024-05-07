c8534c741b3850521ec0808df8c20df97db4a45e
felixparejapmdit <155054492+felixparejapmdit@users.noreply.github.com>
2024-05-07 11:51:15 +0800

    Update musics.blade.php
    update description

--- resources/views/musics.blade.php
+++ resources/views/musics.blade.php
@@ -12,7 +12,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Music List') }}
+            {{ __('Music Listsss') }}
         </h2>
     </x-slot>
 
