4ae29d7a418954c0cd84b4ce5fc2895ef0a94a87
felixparejapmdit <155054492+felixparejapmdit@users.noreply.github.com>
2024-06-04 09:22:22 +0800

    buttons

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -3,6 +3,3 @@
 <label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 dark:text-gray-300']) }}>
     {{ $value ?? $slot }}
 </label>
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
-    {{ $slot }}
-</label>
